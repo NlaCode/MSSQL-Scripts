0dd1322fabda32656f286f3f02374c0c46f8af30
André Luiz Nascimento <uk.nascimento@gmail.com>
2026-04-30 09:15:37 -0300

    feat: adding a more detailed backup script from Microsoft

--- backups/Checking Backups.sql
+++ backups/Checking Backups.sql
@@ -1,49 +1,39 @@
-
-/* Checking backups directories */
-SELECT 
-    bs.database_name,
-    bs.backup_start_date,
-    bs.backup_finish_date,
-    bs.expiration_date,
-    bmf.physical_device_name
-FROM 
-    msdb.dbo.backupset bs
-JOIN msdb.dbo.backupmediafamily bmf ON bs.media_set_id = bmf.media_set_id
-WHERE 
-    bs.type = 'D' -- 'D' indica backup completo (full)
-    AND bs.database_name = 'producao' -- escolha o database
-ORDER BY 
-    bs.backup_finish_date DESC;
-
-/* Checking databases + backup */
-SELECT
-    DB_NAME() AS database_name,
-    CONCAT(CAST(SUM(
-        CAST( (size * 8.0/1024/1024/1024) AS DECIMAL(15,2) )
-    ) AS VARCHAR(20)),' TB') AS database_size
-FROM sys.database_files;
-
-SELECT TOP 1
-msdb.dbo.backupset.database_name,
-msdb.dbo.backupset.backup_finish_date,
-(CAST(msdb.dbo.backupset.backup_size AS NUMERIC(35,2))/1048576.0 / 1024 / 1024) AS backup_size_TB
-FROM msdb.dbo.backupmediafamily
-INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id
-INNER JOIN sys.databases db on db.name = msdb.dbo.backupset.database_name
-WHERE msdb.dbo.backupset.type = 'D'
-and backup_finish_date > dateadd(week,-1,getdate())
-and db.database_id = db_id()
-ORDER BY
-backup_finish_date desc
-
-/* Checking backup erros in agent jobs */
-
-SELECT count(distinct b.name) as Backupfail
-FROM msdb.dbo.sysjobhistory A  JOIN msdb.dbo.sysjobs B ON B.job_id = A.job_id 
-WHERE B.[name] like '% %'  and B.[name] not like '%log%' and a.step_name = 'backup' and a.sql_severity <> 0
-having convert(date,convert(varchar,max(a.run_date)),113) >= convert(date,getdate()-1)
-
--- Consulta detalhada
-SELECT *
-FROM msdb.dbo.sysjobhistory A  JOIN msdb.dbo.sysjobs B ON B.job_id = A.job_id 
-WHERE B.[name] like '% %'  and B.[name] not like '%log%' and a.step_name = 'backup' and a.sql_severity <> 0
+SELECT bs.database_name,
+    backuptype = CASE 
+        WHEN bs.type = 'D' AND bs.is_copy_only = 0 THEN 'Full Database'
+        WHEN bs.type = 'D' AND bs.is_copy_only = 1 THEN 'Full Copy-Only Database'
+        WHEN bs.type = 'I' THEN 'Differential database backup'
+        WHEN bs.type = 'L' THEN 'Transaction Log'
+        WHEN bs.type = 'F' THEN 'File or filegroup'
+        WHEN bs.type = 'G' THEN 'Differential file'
+        WHEN bs.type = 'P' THEN 'Partial'
+        WHEN bs.type = 'Q' THEN 'Differential partial'
+        END + ' Backup',
+    CASE bf.device_type
+        WHEN 2 THEN 'Disk'
+        WHEN 5 THEN 'Tape'
+        WHEN 7 THEN 'Virtual device'
+        WHEN 9 THEN 'Azure Storage'
+        WHEN 105 THEN 'A permanent backup device'
+        ELSE 'Other Device'
+        END AS DeviceType,
+    bms.software_name AS backup_software,
+    bs.recovery_model,
+    bs.compatibility_level,
+    BackupStartDate = bs.Backup_Start_Date,
+    BackupFinishDate = bs.Backup_Finish_Date,
+    LatestBackupLocation = bf.physical_device_name,
+    backup_size_mb = CONVERT(DECIMAL(10, 2), bs.backup_size / 1024. / 1024.),
+    compressed_backup_size_mb = CONVERT(DECIMAL(10, 2), bs.compressed_backup_size / 1024. / 1024.),
+    database_backup_lsn, -- For tlog and differential backups, this is the checkpoint_lsn of the FULL backup it is based on.
+    checkpoint_lsn,
+    begins_log_chain,
+    bms.is_password_protected
+FROM msdb.dbo.backupset bs
+LEFT JOIN msdb.dbo.backupmediafamily bf ON bs.[media_set_id] = bf.[media_set_id]
+INNER JOIN msdb.dbo.backupmediaset bms  ON bs.[media_set_id] = bms.[media_set_id]
+WHERE bs.backup_start_date > DATEADD(MONTH, - 1, sysdatetime()) --only look at last month
+and bs.database_name = ' ' -- filter what backup do you want
+--and bs.type = 'D' -- filter what type of backup do you want
+ORDER BY bs.database_name ASC,
+    bs.Backup_Start_Date DESC;
